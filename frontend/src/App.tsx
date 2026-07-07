ce308480e9c3c255d2189b2134bc280bd6802f62
renanrod4 <renanrdemeneses@gmail.com>
2026-07-06 22:27:26 -0300

    style(pasta): add UI da pagina inicial com o header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import './App.css'
 export default function App() {
   return (
     <div>
-      <h1>Gerenciador de Projetos</h1>
+      {/* <h1>Gerenciador de Despesas</h1> */}
+      <header>
+        <ul>
+          <li>Painel Geral</li>
+          <li>Gerenciar Pessoas</li>
+          <li>Lançar Transações</li>
+        </ul>
+      </header>
+      <main>
+        
+      </main>
     </div>
   );
 }
